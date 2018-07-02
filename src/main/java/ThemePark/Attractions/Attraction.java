7001145cd6a9c418b3d2fa5b2681835aac09c496
StuartCharters <charters.stuart@gmail.com>
2018-07-02 16:05:41 +0100

    can create a dodgems ride and get name as attraction'

--- src/main/java/ThemePark/Attractions/Attraction.java
+++ src/main/java/ThemePark/Attractions/Attraction.java
@@ -1,4 +1,14 @@
 package ThemePark.Attractions;
 
-public class Attraction {
+public abstract class Attraction {
+    private String name;
+
+    public Attraction(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 }
--- src/test/java/ThemePark/Attractions/DodgemsTest.java
+++ src/test/java/ThemePark/Attractions/DodgemsTest.java
@@ -1,4 +1,21 @@
 package ThemePark.Attractions;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemsTest {
+
+    private Dodgems dodgems;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Crashing Cars");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Crashing Cars", dodgems.getName());
+    }
 }
